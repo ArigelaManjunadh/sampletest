2e7deac2e202dee856d899719bb199e798e41e44
ArigelaManjunadh <45787046+ArigelaManjunadh@users.noreply.github.com>
2018-12-25 16:04:51 +0530

    Update SampleJenkinControllerSpec.groovy

--- src/test/groovy/com/jenkins/controller/SampleJenkinControllerSpec.groovy
+++ src/test/groovy/com/jenkins/controller/SampleJenkinControllerSpec.groovy
@@ -13,6 +13,6 @@ class SampleJenkinControllerSpec extends Specification implements ControllerUnit
 
     void "test something"() {
         expect:"fix me"
-            true == false
+            true == true
     }
 }
