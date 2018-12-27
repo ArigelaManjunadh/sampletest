a6abec18d7308f384b65303c4376ef4f76adc83f
MANJUNADH <arigalamanjunath@gmail.com>
2018-12-27 08:30:12 +0530

    removing script folder

--- scripts/_Events.groovy
+++
@@ -1,8 +0,0 @@
-eventCreateWarStart = { warName, stagingDir ->
-    def buildNumber = System.getenv('build.number')
-    if(buildNumber) {
-        ant.propertyfile(file:"${stagingDir}/WEB-INF/classes/application.properties") {
-            entry(key:'buildnumber', value: buildNumber)
-        }
-    }
-}
